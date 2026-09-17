91ada7f013bfff8bfc0de2312a344c80c2b2f82c
yash082014 <yashtripathi082014@gmail.com>
2026-09-17 23:50:40 +0530

    Fix Ruff unused import 1

--- app/lambda_handler.py
+++ app/lambda_handler.py
@@ -1,12 +1,11 @@
 from app.service import get_message
 
-def lambda_handler(event, context):
 
+def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": get_message()
+        "body": get_message(),
     }
-
 
 
 
