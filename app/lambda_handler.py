38af0e15f3e77e7d5ab774704119ed3ab59d6ba3
yash082014 <yashtripathi082014@gmail.com>
2026-09-25 23:08:19 +0530

    testing dev

--- app/lambda_handler.py
+++ app/lambda_handler.py
@@ -4,13 +4,3 @@ from app.service import get_message
 def lambda_handler(event, context):
     return {"statusCode": 200, "body": get_message()}
 
-
-# svsfdvsds
-
-
-# def lambda_handler(event, context):
-
-#     return {
-#         "statusCode": 200,
-#         "body": "Hello Enterprise CI/CD"
-#     }jhg
